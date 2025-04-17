4969c6b4f495a2ea4ae35b3a28d3213ac587fafb
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2025-04-17 17:43:57 +0700

    feat: add feat create of character

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+import { useCreateCharacter } from "../services/useCreateCharacter"
 import { useFetchCharacter } from "../services/useFetchCharacter"
 
 type FormData = {
@@ -7,45 +9,70 @@ type FormData = {
 }
 
 const HomePage = () => {
+    const [showAddCharacter, setShowAddCharacter] = useState(false)
     const { data } = useFetchCharacter()
+    const { mutate } = useCreateCharacter()
 
     return (
-        <div>
-            <div className="container w-3xl mx-auto p-4 mt-10">
-                <div className="flex justify-between items-center mb-4">
-                    <h2 className="text-2xl font-bold text-white">Daftar Data</h2>
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Tambah Data</button>
-                </div>
-
-                <div className="overflow-x-auto">
-                    <table className="min-w-full bg-white border border-gray-200 rounded-lg overflow-hidden">
-                        <thead className="bg-gray-100 text-left">
-                            <tr>
-                                <th className="px-4 py-2 border-b">#</th>
-                                <th className="px-4 py-2 border-b">Nama</th>
-                                <th className="px-4 py-2 border-b">Email</th>
-                                <th className="px-4 py-2 border-b flex justify-center">Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data?.map((character: FormData) => (
-                                <tr key={character.id} className="hover:bg-gray-200">
-                                    <td className="px-4 py-2 border-b">1</td>
-                                    <td className="px-4 py-2 border-b">{character.name}</td>
-                                    <td className="px-4 py-2 border-b">{character.job}</td>
-                                    <td className="px-4 py-2 border-b space-x-2 flex justify-center">
-                                        <button className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600 text-sm">Edit</button>
-                                        <button className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600 text-sm">Hapus</button>
-                                    </td>
-                                </tr>
-                            ))}
-                            
-
-                        </tbody>
-                    </table>
+        <>
+            <div className={`fixed flex justify-center items-center h-screen w-screen top-0 left-0 bg-black/50 z-20 ${showAddCharacter ? '' : "hidden"}`}>
+                <div className="flex justify-center items-center relative w-1/4 gap-8 p-8 bg-white rounded ">
+                    <button className="absolute top-2 right-3 cursor-pointer hover:text-red-500" onClick={() => setShowAddCharacter(false)}>
+                        X
+                    </button>
+                    <form onSubmit={(e) => {
+                        e.preventDefault()
+                        const formData = new FormData(e.target as HTMLFormElement)
+                        const name = formData.get('name') as string
+                        const job = formData.get('job') as string
+                        mutate({ name, job })
+                    }} className="w-full ">
+                        <div className="flex flex-col space-y-4">
+                            <input type="text" name="name" placeholder="Name" className="p-2 border border-gray-300 rounded" />
+                            <input type="text" name="job" placeholder="Job" className="p-2 border border-gray-300 rounded" />
+                            <button type="submit" className="bg-black w-full text-white font-bold px-4 py-2 rounded">Submit</button>
+                        </div>
+                    </form>
                 </div>
             </div>
-        </div>
+            <div>
+                <div className="container w-3xl mx-auto p-4 mt-10  ">
+                    <div className="flex justify-between items-center mb-4">
+                        <h2 className="text-2xl font-bold text-white">Daftar Data</h2>
+                        <button onClick={() => setShowAddCharacter(true)} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Tambah Data</button>
+                    </div>
+
+                    <div className=" h-[500px] overflow-y-auto ">
+                        <table className="min-w-full bg-white border border-gray-200 rounded-lg ">
+                            <thead className="bg-gray-100 text-left sticky top-0 z-10">
+                                <tr>
+                                    <th className="px-4 py-2 border-b">id</th>
+                                    <th className="px-4 py-2 border-b">Nama</th>
+                                    <th className="px-4 py-2 border-b">Email</th>
+                                    <th className="px-4 py-2 border-b flex justify-center">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data?.map((character: FormData) => (
+                                    <tr key={character.id} className="hover:bg-gray-200">
+                                        <td className="px-4 py-2 border-b">{character.id}</td>
+                                        <td className="px-4 py-2 border-b">{character.name}</td>
+                                        <td className="px-4 py-2 border-b">{character.job}</td>
+                                        <td className="px-4 py-2 border-b space-x-2 flex justify-center">
+                                            <button className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600 text-sm">Edit</button>
+                                            <button className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600 text-sm">Hapus</button>
+                                        </td>
+                                    </tr>
+                                ))}
+
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </>
+
     )
 }
 
