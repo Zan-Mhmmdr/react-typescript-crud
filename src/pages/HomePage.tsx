839a7164257289c305e65391c36a1e295b5b108a
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2025-04-17 20:38:00 +0700

    feat: add an animate of modal

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,40 +10,61 @@ type FormData = {
 
 const HomePage = () => {
     const [showAddCharacter, setShowAddCharacter] = useState(false)
+    const [animateModal, setAnimateModal] = useState(false)
     const { data } = useFetchCharacter()
     const { mutate } = useCreateCharacter()
 
+    const handleShowModal = () => {
+        setShowAddCharacter(true)
+        setTimeout(() => {
+            setAnimateModal(true)
+        }, 10);
+    }
+
+    const handleCloseModal = () => {
+        setAnimateModal(false)
+        setTimeout(() => {
+            setShowAddCharacter(false)
+        }, 300);
+    }
+
     return (
         <>
-            <div className={`fixed flex justify-center items-center h-screen w-screen top-0 left-0 bg-black/50 z-20 ${showAddCharacter ? '' : "hidden"}`}>
-                <div className="flex justify-center items-center relative w-1/4 gap-8 p-8 bg-white rounded ">
-                    <button className="absolute top-2 right-3 cursor-pointer hover:text-red-500" onClick={() => setShowAddCharacter(false)}>
-                        X
-                    </button>
-                    <form onSubmit={(e) => {
-                        e.preventDefault()
-                        const formData = new FormData(e.target as HTMLFormElement)
-                        const name = formData.get('name') as string
-                        const job = formData.get('job') as string
-                        mutate({ name, job })
-                    }} className="w-full ">
-                        <div className="flex flex-col space-y-4">
-                            <input type="text" name="name" placeholder="Name" className="p-2 border border-gray-300 rounded" />
-                            <input type="text" name="job" placeholder="Job" className="p-2 border border-gray-300 rounded" />
-                            <button type="submit" className="bg-black w-full text-white font-bold px-4 py-2 rounded">Submit</button>
-                        </div>
-                    </form>
+
+            {showAddCharacter && (
+                <div className={`fixed flex justify-center items-center h-screen w-screen top-0 left-0 bg-black/50 z-20 transition-opacity duration-300 ${animateModal ? "opacity-100" : "opacity-0"}`}>
+                    <div className={`w-1/4 gap-8 p-8 bg-white rounded shadow-lg transform transition-transform duration-300 ${animateModal ? "scale-100" : "scale-95"}`}>
+                        {/* isi modal */}
+
+                        <button className="absolute top-2 right-3 cursor-pointer hover:text-red-500" onClick={handleCloseModal}>
+                            X
+                        </button>
+                        <form onSubmit={(e) => {
+                            e.preventDefault()
+                            const formData = new FormData(e.target as HTMLFormElement)
+                            const name = formData.get('name') as string
+                            const job = formData.get('job') as string
+                            mutate({ name, job })
+                        }} className="w-full ">
+                            <div className="flex flex-col space-y-4">
+                                <input type="text" name="name" placeholder="Name" className="p-2 border border-gray-300 rounded" />
+                                <input type="text" name="job" placeholder="Job" className="p-2 border border-gray-300 rounded" />
+                                <button type="submit" className="bg-black w-full text-white font-bold px-4 py-2 rounded">Submit</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            )}
+
             <div>
                 <div className="container w-3xl mx-auto p-4 mt-10  ">
                     <div className="flex justify-between items-center mb-4">
                         <h2 className="text-2xl font-bold text-white">Daftar Data</h2>
-                        <button onClick={() => setShowAddCharacter(true)} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Tambah Data</button>
+                        <button onClick={handleShowModal} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Tambah Data</button>
                     </div>
 
-                    <div className=" h-[500px] overflow-y-auto ">
-                        <table className="min-w-full bg-white border border-gray-200 rounded-lg ">
+                    <div className=" h-[500px] overflow-y-auto rounded-x">
+                        <table className="min-w-full bg-white border border-gray-200  shadow-2xl">
                             <thead className="bg-gray-100 text-left sticky top-0 z-10">
                                 <tr>
                                     <th className="px-4 py-2 border-b">id</th>
@@ -72,7 +93,6 @@ const HomePage = () => {
                 </div>
             </div>
         </>
-
     )
 }
 
--- src/services/useCreateCharacter.ts
+++ src/services/useCreateCharacter.ts
@@ -1,12 +1,14 @@
 import { axiosInstance } from "../lib/axios"
 import { useMutation } from "@tanstack/react-query"
 
+type payLoadType = {
+    name: string,
+    job: string
+}
+
 export const useCreateCharacter = () => {
     return useMutation({
-        mutationFn: async (payLoad: {
-            name: string,
-            job: string
-        }) => {
+        mutationFn: async (payLoad: payLoadType) => {
             const responseCharacters = await axiosInstance.post("/characters", {
                 name: payLoad.name ? payLoad.name : undefined,
                 job: payLoad.job ? payLoad.job : undefined
