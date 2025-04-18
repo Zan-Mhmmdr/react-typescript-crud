88e142eef9cf4bca0eb07af651b7989fda43f726
Rayzan Muhammad R <rayzanmuhammadr774@gmail.com>
2025-04-18 11:16:14 +0700

    fix: fix bug in a delete char by id

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { useCreateCharacter } from "../services/useCreateCharacter"
 import { useFetchCharacter } from "../services/useFetchCharacter"
 import { useDeleteCharacter } from "../services/useDeleteCharacter"
+import { useEditCharacter } from "../services/useEditCharacter"
 
 type FormData = {
     id: string,
@@ -11,11 +12,15 @@ type FormData = {
 
 const HomePage = () => {
     const [showAddCharacter, setShowAddCharacter] = useState(false)
+    const [showDeleteCharacter, setShowDeleteCharacter] = useState(false)
+    const [showEditCharacter, setShowEditCharacter] = useState(false)
+    const [editData, setEditData] = useState<FormData | null>(null)
     const [animateModal, setAnimateModal] = useState(false)
-    const [confirmDeleteId, setConfirmDeleteId] = useState(false)
+    const [confirmDeleteId, setConfirmDeleteId] = useState<string | null>(null)
     const { data } = useFetchCharacter()
     const { mutate } = useCreateCharacter()
     const { mutate: deleteCharacter, } = useDeleteCharacter()
+    const { mutate: editCharacter } = useEditCharacter()
 
     const handleShowModal = () => {
         setShowAddCharacter(true)
@@ -31,11 +36,16 @@ const HomePage = () => {
         }, 300);
     }
 
-    const handleShowModalDelete = () => {
-        setConfirmDeleteId(true)
+    const handleShowModalDelete = (id: string) => {
+        setShowDeleteCharacter(true)
         setTimeout(() => {
             setAnimateModal(true)
         }, 10);
+
+        if (id) {
+            setConfirmDeleteId(id)
+            return
+        }
     }
 
     return (
@@ -67,15 +77,15 @@ const HomePage = () => {
             )}
 
             {/* Modal buat konfirmasi delete */}
-            {confirmDeleteId && (
-                <div className={`fixed inset-0 z-30 bg-black/50 flex items-center justify-center transition-opacity duration-300 `}>
+            {showDeleteCharacter && (
+                <div className={`fixed inset-0 z-30 bg-black/50 flex items-center justify-center transition-opacity duration-300 ${animateModal ? "opacity-100" : "opacity-0"} `}>
                     <div className="bg-white rounded p-6 w-[90%] max-w-sm shadow-lg">
                         <h2 className="text-xl font-bold mb-4">Yakin ingin menghapus?</h2>
                         <p className="text-gray-600 mb-6">Data yang dihapus tidak bisa dikembalikan.</p>
                         <div className="flex justify-end gap-4">
                             <button
-                                onClick={() => setConfirmDeleteId(null)}
-                                className="px-4 py-2 rounded bg-gray-200 hover:bg-gray-300"
+                                onClick={() => setShowDeleteCharacter(false)}
+                                className="px-4 py-2 rounded bg-gray-200 hover:bg-gray-300 cursor-pointer"
                             >
                                 Batal
                             </button>
@@ -84,11 +94,51 @@ const HomePage = () => {
                                     deleteCharacter(confirmDeleteId);
                                     setConfirmDeleteId(null);
                                 }}
-                                className="px-4 py-2 rounded bg-red-500 text-white hover:bg-red-600"
+                                className="px-4 py-2 rounded bg-red-500 text-white hover:bg-red-600 cursor-pointer"
                             >
                                 Hapus
                             </button>
                         </div>
+                    </div>
+                </div>
+            )}
+
+            {/* Modal buat edit  */}
+            {showEditCharacter && editData && (
+                <div className={`fixed flex justify-center items-center h-screen w-screen top-0 left-0 bg-black/50 z-20 transition-opacity duration-300 ${animateModal ? "opacity-100" : "opacity-0"}`}>
+                    <div className={`w-1/4 gap-8 p-8 bg-white rounded shadow-lg transform transition-transform duration-300 ${animateModal ? "scale-100" : "scale-95"}`}>
+
+                        <button className="absolute top-2 right-3 cursor-pointer hover:text-red-500" onClick={handleCloseModal}>
+                            X
+                        </button>
+
+                        <form onSubmit={(e) => {
+                            e.preventDefault()
+                            const formData = new FormData(e.target as HTMLFormElement)
+                            const name = formData.get('name') as string
+                            const job = formData.get('job') as string
+
+                            // Kirim data yang sudah diedit
+                            editCharacter({ characterID: editData.id, payload: { name, job } })
+                        }} className="w-full">
+                            <div className="flex flex-col space-y-4">
+                                <input
+                                    type="text"
+                                    name="name"
+                                    defaultValue={editData.name}
+                                    placeholder="Name"
+                                    className="p-2 border border-gray-300 rounded"
+                                />
+                                <input
+                                    type="text"
+                                    name="job"
+                                    defaultValue={editData.job}
+                                    placeholder="Job"
+                                    className="p-2 border border-gray-300 rounded"
+                                />
+                                <button type="submit" className="bg-blue-600 w-full text-white font-bold px-4 py-2 rounded">Update</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             )}
@@ -98,7 +148,7 @@ const HomePage = () => {
                 <div className="container w-3xl mx-auto p-4 mt-10  ">
                     <div className="flex justify-between items-center mb-4">
                         <h2 className="text-2xl font-bold text-white">Daftar Data</h2>
-                        <button onClick={handleShowModal} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Tambah Data</button>
+                        <button onClick={handleShowModal} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 cursor-pointer">Tambah Data</button>
                     </div>
 
                     <div className=" h-[500px] overflow-y-auto rounded-x">
@@ -118,8 +168,8 @@ const HomePage = () => {
                                         <td className="px-4 py-2 border-b">{character.name}</td>
                                         <td className="px-4 py-2 border-b">{character.job}</td>
                                         <td className="px-4 py-2 border-b space-x-2 flex justify-center">
-                                            <button className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600 text-sm">Edit</button>
-                                            <button onClick={() => setConfirmDeleteId(character.id)} className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600 text-sm">Hapus</button>
+                                            <button onClick={() => setShowEditCharacter(character.id)} className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600 text-sm cursor-pointer">Edit</button>
+                                            <button onClick={() => handleShowModalDelete(character.id)} className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600 text-sm cursor-pointer">Hapus</button>
                                         </td>
                                     </tr>
                                 ))}
